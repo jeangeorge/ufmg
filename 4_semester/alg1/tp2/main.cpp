9b8b3309832eb6722b3dd4223eb7a8f39497e708
Jean <jean.george.alves@gmail.com>
2019-10-26 22:09:07 -0300

    adding documentation

--- 4_semester/alg1/tp2/main.cpp
+++ 4_semester/alg1/tp2/main.cpp
@@ -4,10 +4,6 @@
 #include "Ilha.hpp"
 #include "Viagem.hpp"
 
-#include <chrono>
-#include <iomanip>
-using namespace std::chrono;
-
 using namespace std;
 
 int main(int argc, char **argv) {
@@ -41,38 +37,8 @@ int main(int argc, char **argv) {
   Viagem *viagem = new Viagem(custo_total, quantidade_ilhas, ilhas);
 
   // Calcula as duas soluções
-
-  double guloso[11], dinamico[11];
-  for (int i = 0; i < 11; i++) {
-    auto start = high_resolution_clock::now();
-
-    viagem->viagemComRepeticao();
-
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    guloso[i] = duration.count();
-  }
-
-  for (int i = 0; i < 11; i++) {
-    auto start = high_resolution_clock::now();
-
-    viagem->viagemSemRepeticao();
-
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    dinamico[i] = duration.count();
-  }
-
-  cout << "TEMPOS GULOSO:" << endl;
-  for (int i = 1; i < 11; i++) {
-    cout << std::setprecision(2) << std::fixed << guloso[i] << " ";
-  }
-  cout << endl;
-  cout << "TEMPOS dinamico:" << endl;
-  for (int i = 1; i < 11; i++) {
-    cout << std::setprecision(2) << dinamico[i] << " ";
-  }
-  cout << endl;
+  viagem->viagemComRepeticao();
+  viagem->viagemSemRepeticao();
 
   delete[] ilhas;
   delete viagem;
