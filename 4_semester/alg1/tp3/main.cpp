d759c2db4c96ca6461951344c505e487a8bb213e
Jean <jean.george.alves@gmail.com>
2019-11-23 21:06:57 -0300

    adicionando medicao de tempo

--- 4_semester/alg1/tp3/main.cpp
+++ 4_semester/alg1/tp3/main.cpp
@@ -1,41 +1,48 @@
 #include <fstream>
 #include <iostream>
 
+#include <chrono>
 #include "Sudoku.hpp"
 
 using namespace std;
 
 int main(int argc, char **argv) {
-  // Abre o arquivo de acordo com o nome recebido no argumento
-  ifstream arquivo(argv[1]);
+  for (int i = 0; i < 10; i++) {
+    auto t1 = std::chrono::high_resolution_clock::now();
 
-  // Se arquivo não foi aberto, já encerra o programa
-  if (!arquivo.is_open()) {
-    return 0;
+    // Abre o arquivo de acordo com o nome recebido no argumento
+    ifstream arquivo(argv[1]);
+
+    // Se arquivo não foi aberto, já encerra o programa
+    if (!arquivo.is_open()) {
+      return 0;
+    }
+
+    // Cria um objeto sudoku, passando o arquivo como parâmetro
+    Sudoku *sudoku = new Sudoku(&arquivo);
+
+    // Fecha o arquivo, se não fechar encerra o programa
+    arquivo.close();
+    if (arquivo.is_open()) {
+      return 0;
+    }
+    // Tenta resolver o sudoku
+    if (sudoku->resolver()) {
+      // cout << "solucao" << endl;
+    } else {
+      // cout << "sem solucao" << endl;
+    }
+
+    // Mostra a solução encontrada.
+    // Caso não encontre solução mostra até onde o algoritmo resolveu.
+    // sudoku->mostrar();
+
+    // Destrói o objeto Sudoku
+    delete sudoku;
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+    std::cout << duration << " ";
   }
-
-  // Cria um objeto sudoku, passando o arquivo como parâmetro
-  Sudoku *sudoku = new Sudoku(&arquivo);
-
-  // Fecha o arquivo, se não fechar encerra o programa
-  arquivo.close();
-  if (arquivo.is_open()) {
-    return 0;
-  }
-
-  // Tenta resolver o sudoku
-  if (sudoku->resolver()) {
-    cout << "solucao" << endl;
-  } else {
-    cout << "sem solucao" << endl;
-  }
-
-  // Mostra a solução encontrada.
-  // Caso não encontre solução mostra até onde o algoritmo resolveu.
-  sudoku->mostrar();
-
-  // Destrói o objeto Sudoku
-  delete sudoku;
-
   return 0;
 }
