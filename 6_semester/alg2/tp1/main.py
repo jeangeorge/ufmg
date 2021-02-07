54471ec1750bcf9d461c79d70498ad3d8c59b9d0
jean.george.alves@gmail.com <jean.george.alves@gmail.com>
2021-02-06 22:04:23 -0300

    Finalizando tp alg2

--- 6_semester/alg2/tp1/main.py
+++ 6_semester/alg2/tp1/main.py
@@ -1,32 +1,73 @@
 import sys
-import os
 
 from trie import Trie
 from utils import *
+from datetime import datetime
+
+BYTE_SIZE = 3
+DECODING_MODE = "utf8"
 
 def compress_file(input_file, output_file):
 	# Tenta abrir arquivo e pegar o texto
 	with open(input_file, "r") as file:
 		text = file.read()
 
-	# Instancia a trie inicial
-	trie = Trie()
+	# Instancia a trie
+	trie = Trie(BYTE_SIZE, DECODING_MODE)
 
+	# Popula a trie com o texto e obtem o texto compactado
 	compressed_text = trie.populate_and_compress(text)
+
+	# Pega o nome do arquivo
 	if output_file:
 		file_name = output_file
 	else:
 		file_name = input_file.replace('.txt', '.z78')
 
+	# Escreve o binario do arquivo comprimido
 	with open(file_name, 'wb') as file:
-		encoded_compressed_text = compressed_text.encode('utf-8')
-		file.write(encoded_compressed_text)
+		file.write(compressed_text)
 
 def decompress_file(input_file, output_file):
-	trie = Trie()
+	# Tenta abrir e ler o arquivo binario
+	with open(input_file, mode="rb") as file:
+		binary = file.read()
+
+	# Dicionario para armazenar as palavras
+	words = {0: ""}
+
+	# O texto descomprimido
+	uncompressed_text = ""
+
+	# Variavel tamanho auxiliar
+	size = 1
+
+	# Percorre o arquivo binario, caractere a caractere andando em passo BYTE_sIZE + 1
+	for i in range(0, len(binary), BYTE_SIZE + 1):
+		# Validacao para nao bugar no fim do arquivo
+		if i + BYTE_SIZE >= len(binary):
+			continue
+		# Pega o "index na trie"
+		index = int.from_bytes(binary[i : i + BYTE_SIZE], byteorder="big")
+		# Pega a palavra
+		value = chr(binary[i + BYTE_SIZE])
+		# Monta a palavra e adiciona no dicionario
+		string = words[index] + value
+		words[size] = string
+		uncompressed_text += string
+		size += 1
+
+	# Define o nome do arquivo de saida
+	if output_file:
+		file_name = output_file
+	else:
+		file_name = input_file.replace('.z78', '.txt')
+
+	# Tenta escrever no arquivo
+	with open(file_name, mode="w") as file:
+		file.write(uncompressed_text[:-1])
 
 # Funcao inicial do programa
-# ./<programa> -c teste.txt >>>>> resulta em >>>>> teste.z78
 def main(argv):
 	# Tenta pegar os argumentos
 	# Menos de tres argumentos indica o erro, nesse caso o programa deve ser encerrado
--- 6_semester/alg2/tp1/trie.py
+++ 6_semester/alg2/tp1/trie.py
@@ -1,12 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from node import Node
 
 class Trie(object):
-	def __init__(self):
+	def __init__(self, byte_size, decoding_mode):
 		self.index = 0
 		self.root = Node("")
+		self.compressed_text = bytearray(0)
+		self.byte_size = byte_size
+		self.decoding_mode = decoding_mode
 
+	# Inserindo uma palavra na trie
 	def insert(self, word):
 		node = self.root
+		# Primeiro tenta achar o ultimo no da palavra na trie, caso ele exista
+		# Se nao existir cria um
 		for char in word:
 			if char in node.children:
 				node = node.children[char]
@@ -14,71 +22,55 @@ class Trie(object):
 				new_node = Node(char)
 				node.children[char] = new_node
 				node = new_node
+		# Incrementa em um para ter comportamento igual a um dic normal
 		self.index += 1
 		node.index = self.index
+		# Define que eh o fim de uma palavra
 		node.is_end = True
 
-	def dfs(self, node, pre):
-		if node.is_end:
-			self.output.append((pre + node.char, node))
-		for child in node.children.values():
-			self.dfs(child, pre + node.char)
+	# Buscando uma palavra na trie
+	def search(self, word):
+		current_node = self.root
+		# Percorre a palavra caractere a caractere
+		for char in word:
+			# Se o caractere nao eh indice de nenhum no filho eh porque nao esta na trie
+			if char not in current_node.children:
+				return False, 0
+			# Se eh, continua buscando
+			current_node = current_node.children[char]
+		# Caso indique o fim de uma palavra, retornamos que a palavra esta na trie e tambem o index
+		if current_node.is_end:
+			return True, current_node.index
+		else:
+			return False, 0
 
-	def search(self, x):
-		node = self.root
-		for char in x:
-			if char in node.children:
-				node = node.children[char]
-			else:
-				return []
-		self.output = []
-		self.dfs(node, x[:-1])
-		return self.output
-
+	# Inserindo os dados na trie e atualizando o texto comprimido
 	def populate_and_compress(self, text):
-		print("INPUT:")
-		print(text)
-		compressed_data = ""
 		word = ''
 		i = 0
+		# Percorre o texto caractere a caractere
 		for char in text:
 			i += 1
 			word += char
-			if not word in dict(self.search(word)):
+			word_substring = word[:-1]
+			# Busca a palavra atual e palavra atual sem o ultimo caractere na trie
+			find_word, index_word = self.search(word)
+			find_word_substring, index_word_substring = self.search(word_substring)
+			if not find_word:
+				# Se nao encontrar a palavra eh preciso inserir na trie
 				self.insert(word)
-				nodes = dict(self.search(word[:-1]))
-				if word[:-1] in nodes:
-					compressed_data += "{}".format(nodes[word[:-1]].index) + word[-1]
-				else:
-					compressed_data += "0" + word[-1]
+				# E tambem setar o correto na compressao
+				self.set_compressed_text(index_word_substring, word[-1])
 				word = ''
 			elif i == len(text):
-				nodes = dict(self.search(word))
-				if word in nodes:
-					compressed_data += "{}".format(nodes[word].index) + word[-1]
-				else:
-					compressed_data += "0" + word[-1]
+				# Se encontrar a palavra e estivermos no fim do arquivo, adicionamos um caractere especial
+				# Isso foi necessario para nao bugar na descompressao
+				self.set_compressed_text(index_word, "ぁ")
 				word = ''
-		print("COMPRESSED:")
-		print(compressed_data)
-		return compressed_data
+		return self.compressed_text
 
-	# def populate(self, text):
-	# 	text = "A_ASA_DA_CASA"
-	# 	compressed_data = []
-	# 	dictionnary = ['']
-	# 	word = ''
-	# 	i = 0
-	# 	for char in text:
-	# 		i += 1
-	# 		word += char
-	# 		if not word in dictionnary:
-	# 			dictionnary.append(word)
-	# 			comp_data.append([dictionnary.index(word[:-1]), word[-1]])
-	# 			word = ''
-	# 		elif i == len(text):
-	# 			comp_data.append([dictionnary.index(word), ''])
-	# 			word = ''
-	# 	print(dictionnary)
-	# 	print(comp_data)
-	# 	return comp_data
+	# Recebe um index e o texto e concatena no array de bytes resultante
+	def set_compressed_text(self, index, text):
+		index_bytes = index.to_bytes(length=self.byte_size, byteorder='big')
+		text_bytes = text.encode(self.decoding_mode)
+		self.compressed_text = b"".join([self.compressed_text, index_bytes, text_bytes])
